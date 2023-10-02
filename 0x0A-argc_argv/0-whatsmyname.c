9b0739d1b4c98f73fcebba273aef5c8269e0577d
peterlubega <peterlubega100@gmail.com>
2023-10-02 08:21:32 -0700

    corrections

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints name folowed by a new line.
- * @argc: type int argument
- * @argv: type char argument of string.
- * Return: none
- * /
+   * main - prints name folowed by a new line.
+    * @argc: type int argument
+     * @argv: type char argument of string.
+      * Return: none
+       */
 int main(int argc, char *argv[])
 {
 		(void) argc;
 
-		printf("%s\n", argv[0]);
+			printf("%s\n", argv[0]);
 
-		return (0);
+				return (0);
 }
