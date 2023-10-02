38661e4651c8ba14411c2e4752f924e7773a4e37
peterlubega <peterlubega100@gmail.com>
2023-10-02 08:37:35 -0700

    corrections

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,18 +2,19 @@
 
 /**
    * main - prints all arguments received.
-    * @argc: type int argument
-     * @argv: type char argument of string.
-      * Return: none
+    * @argc: type int argument (argument count)
+     * @argv: type char argument of string (argument vector).
+      * Return: 0 (indicating successful execution)
        */
 int main(int argc, char *argv[])
 {
-		int x;
+	    int x;
 
-			for (x = 0; x < argc; x++)
-					{
-								printf("%s\n", argv[x]);
-									}
+	        for (x = 0; x < argc; x++)
+			    {
+				            printf("%s\n", argv[x]);
+					        }
 
-				return (0);
+		    return 0;
 }
+
