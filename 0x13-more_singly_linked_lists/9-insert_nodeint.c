15d3b334240624ce3e4d16380033d1c8c588d405
peterlubega <peterlubega100@gmail.com>
2023-10-23 22:39:37 -0700

    function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,49 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * insert_nodeint_at_index - Inserts a new node in a linked
+ * list at a given position.
  *
- * Return: pointer to the new node, or NULL
+ * @head: A pointer to the first node in the list.
+ *
+ * @idx: The index where the new node is added.
+ *
+ * @n: The data to insert in the new node.
+ *
+ * Return: A pointer to the new node, or NULL if insertion fails.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
- unsigned int i;
- listint_t *new;
- listint_t *temp = *head;
+unsigned int i;
+listint_t *new;
+listint_t *temp = *head;
 
- new = malloc(sizeof(listint_t));
- if (!new || !head)
- return (NULL);
+new = malloc(sizeof(listint_t)); /* Allocate memory for the new node */
+if (!new || !head)
+return (NULL);
 
- new->n = n;
- new->next = NULL;
+new->n = n;
+new->next = NULL;
 
- if (idx == 0)
- {
- new->next = *head;
- *head = new;
- return (new);
- }
-
- for (i = 0; temp && i < idx; i++)
- {
- if (i == idx - 1)
- {
- new->next = temp->next;
- temp->next = new;
- return (new);
- }
- else
- temp = temp->next;
- }
-
- return (NULL);
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
 }
 
+for (i = 0; temp && i < idx; i++)
+{
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
+return (new);
+}
+else
+temp = temp->next;
+}
+
+return (NULL); /* Return NULL if insertion fails */
+}
