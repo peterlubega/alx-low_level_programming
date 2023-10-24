cbda3d7eb25a7fe633acc15c7b6054e8383fb704
peterlubega <peterlubega100@gmail.com>
2023-10-23 23:03:11 -0700

    corr

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,33 +9,32 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
- listint_t *temp = *head;
- listint_t *current = NULL;
- unsigned int i = 0;
+    listint_t *temp, *current;
+    unsigned int i;
 
- if (*head == NULL)
- return (-1);
+    if (*head == NULL)  // Check if the list is empty
+        return (-1);
 
- if (index == 0)
- {
- *head = (*head)->next;
- free(temp);
- return (1);
- }
+    if (index == 0)
+    {
+        temp = *head;
+        *head = (*head)->next;
+        free(temp);
+        return (1);
+    }
 
- while (i < index - 1)
- {
- if (!head || !(head->next))
- return (-1);
- head = head->next;
- i++;
- }
+    temp = *head;
+    for (i = 0; i < index - 1 && temp != NULL; i++)
+    {
+        temp = temp->next;
+    }
 
+    if (temp == NULL || temp->next == NULL)
+        return (-1);
 
- current = head->next;
- head->next = current->next;
- free(current);
-
- return (1);
+    current = temp->next;
+    temp->next = current->next;
+    free(current);
+    return (1);
 }
 
